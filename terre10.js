a2b7a81675ceb9288f4a8574dae390de368c3620
bernyfrancois <itoutouberny@gmail.com>
2023-12-18 14:50:56 +0100

    feat: add test 1 file aka "Qui est le nombre premier"

---
+++ terre10.js
@@ -0,0 +1,21 @@
+function isPrimeNumber(numberItem) {
+  const number = parseInt(numberItem)
+
+  if (isNaN(number) || number < 0) {
+    return "Error: please provide a valid number"
+  }
+
+  if (number < 2) {
+    return `${number} is not a prime`
+  }
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return `${number} is not a prime`;
+    }
+  }
+
+  return `${number} is a prime`;
+}
+
+console.log(isPrimeNumber(process.argv[2]))
