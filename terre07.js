b5c1461dc8974b097ec3af3a65c1b4f8562ecd52
bernyfrancois <itoutouberny@gmail.com>
2023-12-18 09:26:39 +0100

    feat: add test 8 file aka "count chart length"

---
+++ terre07.js
@@ -0,0 +1,20 @@
+function displayChartLength(params) {
+  if (params.length > 3) {
+    return "Error: Too much items";
+  }
+
+  if (!isNaN(parseInt(params[2]))) {
+    return "Error: numbers not allowed"
+  }
+
+  let count = 0
+  const textTable = params[2].split("")
+
+  textTable.forEach(letter => {
+    count++
+  })
+
+  return count
+}
+
+console.log(displayChartLength(process.argv))
