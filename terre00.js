b19d992fe9c4ff3fa496fa67c5f4fb3308ba3d1b
bernyfrancois <itoutouberny@gmail.com>
2023-12-20 11:58:15 +0100

    feat: Add UTF-8 encoding to terre00.js and refactor getLetterFromCharCode function.

--- terre00.js
+++ terre00.js
@@ -2,10 +2,15 @@ function getAlphabet() {
   let output = ""
 
   for (let i = 97; i <= 122; i++) {
-    output += String.fromCharCode(i)
+    output += getLetterFromCharCode(i)
   }
 
   return output
+}
+
+
+function getLetterFromCharCode(code) {
+  return String.fromCharCode(code)
 }
 
 console.log(getAlphabet())
